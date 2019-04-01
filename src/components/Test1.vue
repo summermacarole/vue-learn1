4af51161ffc4581cebceb1f1ce33a0114d1e11a0
1740249919@qq.com <1740249919@qq.com>
2019-04-01 22:39:32 +0800

    test1

--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,116 +1,112 @@
 <template>
-  <Table :columns="columns" :data="data">
-    <template slot-scope="{ row, index }" slot="name">
-      <Input type="text" v-model="editName" v-if="editIndex === index" />
-      <span v-else>{{ row.name }}</span>
-    </template>
-
-    <template slot-scope="{ row, index }" slot="age">
-      <Input type="text" v-model="editAge" v-if="editIndex === index" />
-      <span v-else>{{ row.age }}</span>
-    </template>
-
-    <template slot-scope="{ row, index }" slot="birthday">
-      <Input type="text" v-model="editBirthday" v-if="editIndex === index" />
-      <span v-else>{{ getBirthday(row.birthday) }}</span>
-    </template>
-
-    <template slot-scope="{ row, index }" slot="address">
-      <Input type="text" v-model="editAddress" v-if="editIndex === index" />
-      <span v-else>{{ row.address }}</span>
-    </template>
-
-    <template slot-scope="{ row, index }" slot="action">
-      <div v-if="editIndex === index">
-        <Button @click="handleSave(index)">保存</Button>
-        <Button @click="editIndex = -1">取消</Button>
-      </div>
-      <div v-else>
-        <Button @click="handleEdit(row, index)">操作</Button>
-      </div>
-    </template>
-  </Table>
+  <Table border :columns="columns7" :data="data6"></Table>
 </template>
 <script>
 export default {
   data() {
     return {
-      columns: [
+      columns7: [
         {
-          title: "姓名",
-          slot: "name"
+          title: "Name",
+          key: "name",
+          render: (h, params) => {
+            return h("div", [
+              h("Icon", {
+                props: {
+                  type: "person"
+                }
+              }),
+              h("strong", params.row.name)
+            ]);
+          }
         },
         {
-          title: "年龄",
-          slot: "age"
+          title: "Age",
+          key: "age"
         },
         {
-          title: "出生日期",
-          slot: "birthday"
+          title: "Address",
+          key: "address"
         },
         {
-          title: "地址",
-          slot: "address"
-        },
-        {
-          title: "操作",
-          slot: "action"
+          title: "Action",
+          key: "action",
+          width: 150,
+          align: "center",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "primary",
+                    size: "small"
+                  },
+                  style: {
+                    marginRight: "5px"
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.index);
+                    }
+                  }
+                },
+                "View"
+              ),
+              h(
+                "Button",
+                {
+                  props: {
+                    type: "error",
+                    size: "small"
+                  },
+                  on: {
+                    click: () => {
+                      this.remove(params.index);
+                    }
+                  }
+                },
+                "Delete"
+              )
+            ]);
+          }
         }
       ],
-      data: [
+      data6: [
         {
-          name: "王小明",
+          name: "John Brown",
           age: 18,
-          birthday: "919526400000",
-          address: "北京市朝阳区芍药居"
+          address: "New York No. 1 Lake Park"
         },
         {
-          name: "张小刚",
-          age: 25,
-          birthday: "696096000000",
-          address: "北京市海淀区西二旗"
+          name: "Jim Green",
+          age: 24,
+          address: "London No. 1 Lake Park"
         },
         {
-          name: "李小红",
+          name: "Joe Black",
           age: 30,
-          birthday: "563472000000",
-          address: "上海市浦东新区世纪大道"
+          address: "Sydney No. 1 Lake Park"
         },
         {
-          name: "周小伟",
+          name: "Jon Snow",
           age: 26,
-          birthday: "687024000000",
-          address: "深圳市南山区深南大道"
+          address: "Ottawa No. 2 Lake Park"
         }
-      ],
-      editIndex: -1, // 当前聚焦的输入框的行数
-      editName: "", // 第一列输入框，当然聚焦的输入框的输入内容，与 data 分离避免重构的闪烁
-      editAge: "", // 第二列输入框
-      editBirthday: "", // 第三列输入框
-      editAddress: "" // 第四列输入框
+      ]
     };
   },
   methods: {
-    handleEdit(row, index) {
-      this.editName = row.name;
-      this.editAge = row.age;
-      this.editAddress = row.address;
-      this.editBirthday = row.birthday;
-      this.editIndex = index;
+    show(index) {
+      this.$Modal.info({
+        title: "User Info",
+        content: `Name：${this.data6[index].name}<br>Age：${
+          this.data6[index].age
+        }<br>Address：${this.data6[index].address}`
+      });
     },
-    handleSave(index) {
-      this.data[index].name = this.editName;
-      this.data[index].age = this.editAge;
-      this.data[index].birthday = this.editBirthday;
-      this.data[index].address = this.editAddress;
-      this.editIndex = -1;
-    },
-    getBirthday(birthday) {
-      const date = new Date(parseInt(birthday));
-      const year = date.getFullYear();
-      const month = date.getMonth() + 1;
-      const day = date.getDate();
-      return `${year}-${month}-${day}`;
+    remove(index) {
+      this.data6.splice(index, 1);
     }
   }
 };
