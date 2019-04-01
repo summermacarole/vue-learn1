d1ff729e11d5928249217d7b99b1c4eec56f5be9
1740249919@qq.com <1740249919@qq.com>
2019-04-01 22:38:29 +0800

    test1

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import iView from "iview";
+import "iview/dist/styles/iview.css";
 
 Vue.config.productionTip = false;
+Vue.use(iView, {
+  size: "mini"
+});
 
 new Vue({
+  el: "#app",
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Test></Test>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Test from "../components/Test1";
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Test
   }
 };
 </script>
