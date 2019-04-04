57678245ec44a60ebb6b4456d4c209c6aedb363b
1740249919@qq.com <1740249919@qq.com>
2019-04-05 00:21:02 +0800

    正常跑通了，可以测试axios了

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 const Koa = require("koa");
 const app = new Koa();
+const bodyParser = require("koa-bodyparser");
 const logger = require("koa-logger");
 const routers = require("./routers");
 
 app.use(logger());
+app.use(bodyParser());
 app.use(routers.routes()).use(routers.allowedMethods());
 app.listen(3000);
--- app/routers/user.js
+++ app/routers/user.js
@@ -3,7 +3,6 @@ const user = new Router();
 const userList = require("../../mock/users");
 
 user.get("/list", async ctx => {
-  console.log(JSON.stringify(ctx.query));
   let { current, size } = ctx.query;
   ctx.body = {
     rows: userList.slice((current - 1) * size, current * size),
@@ -11,4 +10,12 @@ user.get("/list", async ctx => {
   };
 });
 
+user.post("/list", async ctx => {
+  console.log(ctx.request.body);
+  let { current, size } = ctx.request.body;
+  ctx.body = {
+    rows: userList.slice((current - 1) * size, current * size),
+    total: userList.length
+  };
+});
 module.exports = user;
